d0ddbdb76db34ea1686505a33bff834fb0d691f1
Dragon843 <130922507+Dragon843@users.noreply.github.com>
2025-01-16 20:15:11 +0100

    Już koncówka

--- Mobilki_Dronki_2.0/Assets/Scripts/PlayerInput.cs
+++ Mobilki_Dronki_2.0/Assets/Scripts/PlayerInput.cs
@@ -1,40 +1,46 @@
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
-    public Rigidbody rb;
+    public Rigidbody playerBody;
 
     [Header("UI Elements")]
-    public Slider angleXSlider;
-    public Slider thrustPowerSlider;
+    public Slider sliderAngleX;
+    public Slider sliderThrustPower;
+    public Button buttonLeftRotateZ;
+    public Button buttonRightRotateZ;
 
     [Header("Flight settings")]
     [SerializeField]
-    private float thrustForcePlayer; //Siła lotu drona
+    private float playerThrustForce = 30f; //Siła lotu drona
     [SerializeField]
-    private float rotationSpeedX; //Prędkość zmiany nachylenia w X
+    private float playerAngleX = 65f; //Maksymalne nachylenie w osi X
     [SerializeField]
-    private float rotationSpeedY; //Czulosc przechylenia telefonu
+    private float playerAngleZ = 65f; //Maksymalne nachylenie w osi Z
     [SerializeField]
-    private float rotationSpeedZ; //Prędkość zmiany nachylenia w Z
-    private float rotationPhone;
-    private float yTransform;
-    private float xTransform;
-    private float zTransform;
+    private float playerRotateZ;
 
-    
+    private float phoneRotation; //
+    private float yTransformVec;
+    private float xTransformRot;
+    private float zTransformRot;
+
+    private bool buttonLeftPressed = false;
+    private bool buttonRightPressed = false;
+
+    private Vector3 droneRotation;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        playerBody = GetComponent<Rigidbody>();
 
-        thrustPowerSlider.onValueChanged.AddListener(OnThrPowSliderChanged);
-        angleXSlider.onValueChanged.AddListener(OnAngleXSliderChanged);
+        sliderThrustPower.onValueChanged.AddListener(OnThrPowSliderChanged);
+        sliderAngleX.onValueChanged.AddListener(OnAngleXSliderChanged);
 
-        thrustPowerSlider.onValueChanged.AddListener(OnThrPowSliderChanged);
-        angleXSlider.onValueChanged.AddListener(OnAngleXSliderChanged);
+        droneRotation = transform.rotation.eulerAngles;
 
         //Wlaczamy akcelerometr
         Input.gyro.enabled = true;
@@ -46,7 +52,7 @@ public class PlayerInput : MonoBehaviour
         }
     }
 
-    public void Update()
+    private void Update()
     {
         if (transform.position.y <= -10f)
         {
@@ -55,8 +61,8 @@ public class PlayerInput : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
-        rb.AddRelativeForce(0f, yTransform * thrustForcePlayer * Time.deltaTime - Physics.gravity.y, 0f, ForceMode.Acceleration);
+    {        
+        playerBody.AddRelativeForce(0f, yTransformVec * playerThrustForce * Time.fixedDeltaTime - Physics.gravity.y, 0f, ForceMode.Acceleration);
         gyroControll();
         RotationControll();
     }
@@ -65,11 +71,11 @@ public class PlayerInput : MonoBehaviour
     {
         if(value <= 0.2f && value >= -0.2)
         {
-            yTransform = 0;
+            yTransformVec = 0;
         }
         else
         {
-            yTransform = value;
+            yTransformVec = value;
         }
         
     } 
@@ -78,33 +84,81 @@ public class PlayerInput : MonoBehaviour
     {
         if(value <= 0.2f && value >= -0.2f)
         {
-            xTransform = 0f;
+            xTransformRot = 0f;
         }
         else
         {
-            xTransform = value;
+            xTransformRot = value;
         }
+    }
+
+    private void OnButtonLeftDown()
+    {
+        buttonLeftPressed = true;
+    }
+
+    private void OnButtonLeftUp()
+    {
+
+    }
+
+    private void OnButtonRightDown()
+    {
+
+    }
+
+    private void OnButtonRightUp()
+    {
+        
+    }
+
+    private void OnClickLeftButton()
+    {
+        droneRotation.x = transform.rotation.x;
+        droneRotation.z = transform.rotation.z;
+        droneRotation.y += playerRotateZ * Time.fixedDeltaTime;
+
+        transform.rotation = Quaternion.Euler(droneRotation);
+    }
+
+    private void OnClickRightButton()
+    {
+        droneRotation.x = transform.rotation.x;
+        droneRotation.z = transform.rotation.z;
+        droneRotation.y -= playerRotateZ;
+
+        transform.rotation = Quaternion.Euler(droneRotation);
     }
 
     private void gyroControll()
     {
-        rotationPhone = Input.gyro.attitude.eulerAngles.z;
-        if(rotationPhone >= 252f && rotationPhone <= 288f){
-            zTransform = 0f;
+        phoneRotation = Input.gyro.attitude.eulerAngles.z;
+        if(phoneRotation >= 255f && phoneRotation <= 285f){
+            zTransformRot = 0f;
         }
-        else if(rotationPhone <= 360 && rotationPhone >= 180)
+        else if(phoneRotation <= 360 && phoneRotation > 285f || phoneRotation < 255f && phoneRotation >= 180)
         {
-            zTransform = rotationPhone - 270f;
+            zTransformRot = phoneRotation - 270f;
         }
+
+        Debug.Log(phoneRotation);
     }
+
 
     private void RotationControll()
     {
-        Vector3 rotation = transform.rotation.eulerAngles;
-        rotation.x = xTransform * rotationSpeedX * Time.deltaTime;
-        rotation.z = zTransform;
-        transform.rotation = Quaternion.Euler(rotation);
+        //Quaternion stabilzeRotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+        
+        
+        droneRotation.x = xTransformRot * playerAngleX * 45f * Time.fixedDeltaTime;
+        droneRotation.z = zTransformRot * playerAngleZ * Time.fixedDeltaTime;
 
-        Debug.Log("żyroskop: " + Input.gyro.attitude.eulerAngles.z);
+        
+        //droneRotation.y = transform.rotation.z;
+
+        transform.rotation = Quaternion.Euler(droneRotation);
+        
+        
+        //Debug.Log("żyroskop: " + Input.gyro.attitude.eulerAngles.z);
     }
 }
