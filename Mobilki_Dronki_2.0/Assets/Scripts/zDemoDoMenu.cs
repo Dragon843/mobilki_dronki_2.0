b7beb22dae9a1ff43fddce89df5cce84b96d9ac1
bartekszy123 <bartokszy@gmail.com>
2025-01-17 16:52:18 +0100

    dzialajacy przycisk do wracania z gry do menu i prawidlowy obrazek

---
+++ Mobilki_Dronki_2.0/Assets/Scripts/zDemoDoMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zDemoDoMenu : MonoBehaviour
+{
+    public void DemoDoMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
