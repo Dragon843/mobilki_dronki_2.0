4035c29704a252c77e6418fbf5304ab828e92f12
Dragon843 <130922507+Dragon843@users.noreply.github.com>
2025-01-19 15:55:47 +0100

    Przywracanie danych

---
+++ Mobilki_Dronki_2.0/Assets/Scripts/wygrana.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wygrana : MonoBehaviour { 
+
+
+   public void OnTriggerEnter(Collider other) 
+   {
+        if (other.CompareTag("Dron"))
+        {
+            SceneManager.LoadScene("wygrales");
+        }
+        else if (other.CompareTag("DronBOT")) 
+        {
+            SceneManager.LoadScene("przegrales");
+        }
+
+   } 
+
+}
